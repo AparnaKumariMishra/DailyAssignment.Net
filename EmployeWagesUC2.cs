ef10967e5ffd6fd7e9f5937b210982aad843f617
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-09-30 22:39:43 +0530

    Add files via upload
    Added UC2

---
+++ EmployeWagesUC2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePresent
+{
+    public class EmployeWagesUC2
+    {
+      public static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int EmpHRS = 0;
+            int EmpWeg = 0;
+            Random random = new Random();   
+            int EmpCheck = random.Next(0,2);
+            if (EmpCheck == IS_FULL_TIME)
+            {
+                EmpHRS = 8;
+            }
+            else
+            {
+                EmpHRS = 0;
+            }
+            EmpWeg = EmpHRS * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage:" +EmpWeg);
+        }
+
+    }
+}
