1be10bc7291e3a35579f60801bdd6cb27467a659
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-06 11:05:29 +0530

    Add files via upload
    Added UC4

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class UC4
+{
+    public const int Is_Part_time = 1;
+    public const int Is_Full_time = 2;
+    public const int Emp_Rate_Per_Hour = 20;
+    static void Main(string[] args)
+    {
+        int empHrs = 0;
+        int empwags = 0;
+        Random rand = new Random();
+        int empcheck = rand.Next(0, 3);
+        Console.WriteLine("emp wage= " + empwags);
+    }
+}
